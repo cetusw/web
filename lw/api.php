ef368a09f732da27ec54558a1468772e27d92db2
cetusw <kugelev.m@gmail.com>
2024-04-10 17:52:49 +0300

    Сохранение поста через json в базу данных (без картинок)

--- lw/api.php
+++ lw/api.php
@@ -1,4 +1,10 @@
 <?php
+
+const HOST = 'localhost';
+const USERNAME = 'yogurt';
+const PASSWORD = 'pAssw0rd#';
+const DATABASE = 'blog';
+
 function saveFile(string $file, string $data): void
 {
 	$myFile = fopen($file, 'w');
@@ -23,11 +29,44 @@ function saveImage(string $imageBase64)
 	saveFile(__DIR__ . "/src/images/image.{$imgExtension}", $imageDecoded);
 }
 
+function createDBConnection(): mysqli {
+	$conn = new mysqli(HOST, USERNAME, PASSWORD, DATABASE);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	return $conn;
+}
+
+function closeDBConnection(mysqli $conn): void {
+	$conn->close();
+}
+
+function savePostToDatabase(mysqli $conn, $data): bool {
+	$sql = "INSERT INTO post (title, subtitle, content, author, author_url, publish_date, image_url, featured, adventure)
+					VALUES ( 
+					        '{$data['title']}',
+					        '{$data['subtitle']}',
+					        '{$data['content']}',
+					        '{$data['author']}',
+					        '{$data['author_url']}',
+					        '{$data['publish_date']}',
+					        '{$data['image_url']}',
+					        '{$data['featured']}',
+					        '{$data['adventure']}'
+					        );";
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	return $conn->query($sql);
+}
+
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method === 'POST') {
+	$connection = createDBConnection();
 	$dataAsJson = file_get_contents("php://input");
 	$dataAsArray = json_decode($dataAsJson, true);
-	saveImage($dataAsArray['image']);
+	savePostToDatabase($connection, $dataAsArray);
+	echo 'save';
+	closeDBConnection($connection);
 } else {
 	echo 'Метод не POST';
 }
