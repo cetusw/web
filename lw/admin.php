6a2e42069dc4b513c92914c8c68c0524fde03e2c
cetusw <kugelev.m@gmail.com>
2024-04-26 20:10:32 +0300

    Сделал градиент | Добавил анимацию кнопке "Publish"

--- lw/admin.php
+++ lw/admin.php
@@ -14,7 +14,7 @@
     <a href="/admin"><img class="header-block__logo" src="static/images/escape-author.svg" alt="Logo"></a>
     <div class="header-block__account">
       <div class="header-block__author-image">N</div>
-      <a href=""><img class="header-block__logout-icon" src="static/images/log-out.svg" alt="Logout icon"></a>
+      <a href="/home"><img class="header-block__logout-icon" src="static/images/log-out.svg" alt="Logout icon"></a>
     </div>
   </div>
 </header>
@@ -23,27 +23,29 @@
   <div class="post-title container">
     <div class="post-title__text">
       <h1>New Post</h1>
-      <h2>Fill out the form bellow and publish your article</h2>
+      <p>Fill out the form bellow and publish your article</p>
     </div>
-    <button id="button" class="publish">Publish</button>
+    <div class="post-title__button">
+      <button id="button" class="publish">Publish</button>
+    </div>
   </div>
   <div class="main-information container">
     <h2>Main Information</h2>
     <div class="main-information__content">
       <form class="main-information__form">
-        <label>Title<br><input id="input" name="title" type="text" placeholder="New Post" /></label>
-        <label>Short description<br><input id="input" name="description" type="text" placeholder="Please, enter any description" /></label>
-        <label>Author name<br><input id="input" name="author-name" type="text" /></label>
+        <label>Title<br><input id="input" name="title" type="text" placeholder="New Post" maxlength="30"></label>
+        <label>Short description<br><input id="input" name="description" type="text" placeholder="Please, enter any description" maxlength="30"></label>
+        <label>Author name<br><input id="input" name="author-name" type="text" maxlength="30"></label>
         <label>
           Author Photo<br>
-          <img class="input" src="static/images/placeholder-image-round.svg" alt="Photo">
-          <div class="input">
-            <input id="input" name="author-image" type="file"/>
-            <span class="input">Upload</span>
-        </div>
+          <div class="main-information__author-image-upload">
+            <img class="main-information__author-image" src="static/images/placeholder-image-round.svg" alt="Photo">
+            <input id="input" name="author-image" type="file">
+            <span>Upload</span>
+          </div>
         </label>
         <label>Publish Date<br><input id="input" name="publish-date" type="date" /></label>
-        <label>
+        <label class="main-information__hero-image">
           <span>Hero Image</span><br>
           <img class="main-information__hero-image-10mb" src="static/images/placeholder-image-rectangle-10mb.svg" alt="Photo">
           <input id="input" name="hero-image" type="file"/><br>
@@ -57,31 +59,35 @@
         </label>
       </form>
       <div class="main-information__preview">
-        <h3>Article preview</h3>
-        <div class="main-information__article-outer">
-          <div class="main-information__article-inner">
-            <div class="main-information__article-header">
-              <div class="main-information__bullet"></div>
-              <div class="main-information__bullet"></div>
-              <div class="main-information__bullet"></div>
+        <p class="main-information__preview-title">Article preview</p>
+        <div class="main-information__bottom-gradient-article">
+          <div class="main-information__article-outer">
+            <div class="main-information__article-inner">
+              <div class="main-information__article-header">
+                <div class="main-information__bullet"></div>
+                <div class="main-information__bullet"></div>
+                <div class="main-information__bullet"></div>
+              </div>
+              <p class="main-information__article-title">New Post</p>
+              <p class="main-information__article-subtitle">Please, enter any description</p>
+              <img src="static/images/article-preview.svg" alt="Article preview">
             </div>
-            <h5>New Post</h5>
-            <h6>Please, enter any description</h6>
-            <img src="static/images/article-preview.svg">
           </div>
         </div>
-        <h4>Post card preview</h4>
-        <div class="main-information__post-card-preview">
-          <div class="main-information__post">
-            <img class="main-information__author-image-preview" src="static/images/post-card-preview.svg">
-            <div class="main-information__post-header">
-              <h5>New Post</h5>
-              <h6>Please, enter any description</h6>
-            </div>
-            <div class="main-information__post-footer">
-              <img src="static/images/author-image-preview.svg">
-              <span class="main-information__author-name">Enter author name</span>
-              <span class="main-information__publish-date">4/19/2023</span>
+        <p class="main-information__preview-title">Post card preview</p>
+        <div class="main-information__bottom-gradient-post">
+          <div class="main-information__post-card-preview">
+            <div class="main-information__post">
+              <img class="main-information__author-image-preview" src="static/images/post-card-preview.svg" alt="Author Image">
+              <div class="main-information__post-header">
+                <p class="main-information__post-title">New Post</p>
+                <p class="main-information__post-subtitle">Please, enter any description</p>
+              </div>
+              <div class="main-information__post-footer">
+                <img src="static/images/author-image-preview.svg" alt="Author Image">
+                <span class="main-information__author-name">Enter author name</span>
+                <span class="main-information__publish-date">4/19/2023</span>
+              </div>
             </div>
           </div>
         </div>
@@ -89,7 +95,7 @@
     </div>
   </div>
   <div class="content container">
-    <h1>Content</h1>
+    <p>Content</p>
     <label>Post content (plain text)<br><textarea class="content__textarea" name="post-content" placeholder="Type anything you want ..."></textarea></label>
   </div>
 </main>
