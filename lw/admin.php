237ca77c4d33c043f154c9f158151d61e05fa340
cetusw <kugelev.m@gmail.com>
2024-05-09 18:36:34 +0300

    Перенёс все в форму | Добавил отображение картинка автора в превью | Поправил отображение элементов при загрузке картинки | Удаление картикни автора

--- lw/admin.php
+++ lw/admin.php
@@ -20,80 +20,90 @@
 </header>
 
 <main>
-  <div class="post-title container">
-    <div class="post-title__text">
-      <h1>New Post</h1>
-      <p>Fill out the form bellow and publish your article</p>
+  <form class="main-information__form">
+    <div class="post-title container">
+      <div class="post-title__text">
+        <h1>New Post</h1>
+        <p>Fill out the form bellow and publish your article</p>
+      </div>
+      <div class="post-title__button">
+        <button id="button" class="publish">Publish</button>
+      </div>
     </div>
-    <div class="post-title__button">
-      <button id="button" class="publish">Publish</button>
-    </div>
-  </div>
-  <div class="main-information container">
-    <h2>Main Information</h2>
-    <div class="main-information__content">
-      <form class="main-information__form">
-        <label>Title<br><input id="title-input" name="title" type="text" placeholder="New Post" maxlength="30"></label>
-        <label>Short description<br><input id="description-input" name="description" type="text" placeholder="Please, enter any description" maxlength="30"></label>
-        <label>Author name<br><input id="author-input" name="author-name" type="text" maxlength="30"></label>
-        <label>
-          Author Photo<br>
-          <div class="main-information__author-image-upload" id="author-image">
-            <img class="main-information__author-image" src="static/images/placeholder-image-round.svg" alt="Photo">
-            <input id="author-image-input" name="author-image" type="file">
-            <span>Upload</span>
+    <div class="main-information container">
+      <h2>Main Information</h2>
+        <div class="main-information__content">
+          <div class="main-information__inputs">
+            <label>Title<br><input id="title-input" name="title" type="text" placeholder="New Post" maxlength="30" required></label>
+            <label>Short description<br><input id="description-input" name="description" type="text" placeholder="Please, enter any description" maxlength="30" required></label>
+            <label>Author name<br><input id="author-input" name="author-name" type="text" maxlength="30" required></label>
+            <label>
+              Author Photo<br>
+              <input id="author-image-input" name="author-image" type="file" required>
+              <div class="main-information__author-image-upload">
+                <img class="main-information__author-image" src="static/images/placeholder-image-round.svg" id="author-image" alt="Photo">
+                <button class="main-information__upload-new-avatar" id="upload-new-avatar" hidden>
+                  <img src="static/images/camera.svg" alt="camera">
+                  <span>Upload New</span>
+                </button>
+                <button class="main-information__remove-avatar" id="remove-avatar" hidden>
+                  <img src="static/images/trash-red.svg" alt="camera">
+                  <span>Remove</span>
+                </button>
+                <span class="main-information__author-image-upload-text" id="upload">Upload</span>
+              </div>
+            </label>
+            <label>Publish Date<br><input id="date-input" name="publish-date" type="date" required></label>
+            <label class="main-information__hero-image">
+              <span>Hero Image</span><br>
+              <img class="main-information__hero-image-10mb" src="static/images/placeholder-image-rectangle-10mb.svg" alt="Photo">
+              <input id="image10mb-input" name="hero-image" type="file" required><br>
+              <span class="main-information__ps">Size up to 10mb. Format: png, jpeg, gif.</span>
+            </label>
+            <label>
+              <span>Hero Image</span><br>
+              <img class="main-information__hero-image-5mb" src="static/images/placeholder-image-rectangle-5mb.svg" id="image5mb" alt="Photo">
+              <input id="image5mb-input" name="hero-image-small" type="file" required><br>
+              <span class="main-information__ps">Size up to 5mb. Format: png, jpeg, gif.</span>
+            </label>
           </div>
-        </label>
-        <label>Publish Date<br><input id="date-input" name="publish-date" type="date" /></label>
-        <label class="main-information__hero-image">
-          <span>Hero Image</span><br>
-          <img class="main-information__hero-image-10mb" src="static/images/placeholder-image-rectangle-10mb.svg" alt="Photo">
-          <input id="image10mb-input" name="hero-image" type="file"/><br>
-          <span class="main-information__ps">Size up to 10mb. Format: png, jpeg, gif.</span>
-        </label>
-        <label>
-          <span>Hero Image</span><br>
-          <img class="main-information__hero-image-5mb" src="static/images/placeholder-image-rectangle-5mb.svg" alt="Photo">
-          <input id="image5mb-input" name="hero-image-small" type="file"/><br>
-          <span class="main-information__ps">Size up to 5mb. Format: png, jpeg, gif.</span>
-        </label>
-      </form>
-      <div class="main-information__preview">
-        <p class="main-information__preview-title">Article preview</p>
-        <div class="main-information__article-outer">
-          <div class="main-information__article-inner">
-            <div class="main-information__article-header">
-              <div class="main-information__bullet"></div>
-              <div class="main-information__bullet"></div>
-              <div class="main-information__bullet"></div>
+          <div class="main-information__preview">
+            <p class="main-information__preview-title">Article preview</p>
+            <div class="main-information__article-outer">
+              <div class="main-information__article-inner">
+                <div class="main-information__article-header">
+                  <div class="main-information__bullet"></div>
+                  <div class="main-information__bullet"></div>
+                  <div class="main-information__bullet"></div>
+                </div>
+                <p class="main-information__article-title" id="article-preview-title">New Post</p>
+                <p class="main-information__article-description" id="article-preview-description">Please, enter any description</p>
+                <img class="main-information__article-image-preview" src="static/images/article-preview.svg" id="article-image-preview" alt="Article preview">
+              </div>
             </div>
-            <p class="main-information__article-title" id="article-preview-title"></p>
-            <p class="main-information__article-description" id="article-preview-description"></p>
-            <img src="static/images/article-preview.svg" alt="Article preview">
-          </div>
-        </div>
-        <p class="main-information__preview-title">Post card preview</p>
-        <div class="main-information__post-card-preview">
-          <div class="main-information__post">
-            <img class="main-information__author-image-preview" src="static/images/post-card-preview.svg" alt="Author Image">
-            <div class="main-information__post-header">
-              <p class="main-information__post-title" id="post-preview-title"></p>
-              <p class="main-information__post-description" id="post-preview-description"></p>
-            </div>
-            <div class="main-information__post-footer">
-              <img src="static/images/author-image-preview.svg" alt="Author Image">
-              <span class="main-information__author-name" id="post-preview-author"></span>
-              <span class="main-information__publish-date" id="post-preview-date"></span>
+            <p class="main-information__preview-title">Post card preview</p>
+            <div class="main-information__post-card-preview">
+              <div class="main-information__post">
+                <img class="main-information__card-image-preview" src="static/images/post-card-preview.svg" id="card-image-preview" alt="Author Image">
+                <div class="main-information__post-header">
+                  <p class="main-information__post-title" id="post-preview-title">New Post</p>
+                  <p class="main-information__post-description" id="post-preview-description">Please, enter any description</p>
+                </div>
+                <div class="main-information__post-footer">
+                  <img src="static/images/author-image-preview.svg" id="author-image-preview" alt="Author Image">
+                  <span class="main-information__author-name" id="post-preview-author">Enter author name</span>
+                  <span class="main-information__publish-date" id="post-preview-date">00/00/0000</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+    <div class="content container">
+      <p>Content</p>
+      <label>Post content (plain text)<br><textarea class="content__textarea" name="post-content" placeholder="Type anything you want ..."></textarea></label>
     </div>
-  </div>
-  <div class="content container">
-    <p>Content</p>
-    <label>Post content (plain text)<br><textarea class="content__textarea" name="post-content" placeholder="Type anything you want ..."></textarea></label>
-  </div>
+  </form>
 </main>
 
 <footer class="footer-block">
--- lw/src/js/admin.js
+++ lw/src/js/admin.js
@@ -1,3 +1,14 @@
+let postData = {
+    title: '',
+    description: '',
+    authorName: '',
+    authorPhoto: '',
+    publishDate: '',
+    heroImagePost: '',
+    heroImageCard: '',
+}
+
+
 const inputs = document.querySelectorAll('input');
 
 function inputFocus(event) {
@@ -20,27 +31,61 @@ for (const input of inputs) {
 
 }
 
+//titles
 const inputTitle = document.getElementById('title-input');
 const articlePreviewTitle = document.getElementById('article-preview-title');
 const postPreviewTitle = document.getElementById('post-preview-title');
+
+//descriptions
+const inputDescription = document.getElementById('description-input');
+const articlePreviewDescription = document.getElementById('article-preview-description');
+const postPreviewDescription = document.getElementById('post-preview-description');
+
+//author
+const inputAuthor = document.getElementById('author-input')
+const postPreviewAuthor = document.getElementById('post-preview-author');
+
+//date
+const inputDate = document.getElementById('date-input');
+const postPreviewDate = document.getElementById('post-preview-date');
+
+//images
+const authorImageInput = document.getElementById('author-image-input');
+const authorImage = document.getElementById('author-image');
+const articleImageInput = document.getElementById('image10mb-input');
+const articleImagePreview = document.getElementById('article-image-preview');
+const cardImageInput = document.getElementById('image5mb-input');
+const cardImagePreview = document.getElementById('card-image-preview');
+const authorImagePreview = document.getElementById('author-image-preview');
+
+//buttons
+const uploadNewButton = document.getElementById('upload-new-avatar');
+const removeButton = document.getElementById('remove-avatar');
+const upload = document.getElementById('upload');
+
+function initListeners() {
+    inputTitle.addEventListener('input', changeTitle);
+    inputDescription.addEventListener('input', changeDescription);
+    inputAuthor.addEventListener('input', changeAuthor);
+    inputDate.addEventListener('input', changeDate);
+    authorImageInput.addEventListener('change', updateAuthorImageDisplay);
+    removeButton.addEventListener('click', removeAuthorImage);
+    uploadNewButton.addEventListener('click', uploadNewImage);
+    articleImageInput.addEventListener('click', updateArticleImageDispaly);
+}
 
 function changeTitle(event) {
     if (event.target.value !== '') {
         articlePreviewTitle.textContent = event.target.value;
         postPreviewTitle.textContent = event.target.value;
+        postData.title = event.target.value;
     } else {
         articlePreviewTitle.textContent = 'New Post';
         postPreviewTitle.textContent = 'New Post';
+        postData.title = event.target.value;
+
     }
 }
-
-articlePreviewTitle.textContent = 'New Post';
-postPreviewTitle.textContent = 'New Post';
-inputTitle.addEventListener('input', changeTitle);
-
-const inputDescription = document.getElementById('description-input');
-const articlePreviewDescription = document.getElementById('article-preview-description');
-const postPreviewDescription = document.getElementById('post-preview-description');
 
 function changeDescription(event) {
     if (event.target.value !== '') {
@@ -52,13 +97,6 @@ function changeDescription(event) {
     }
 }
 
-articlePreviewDescription.textContent = 'Please, enter any description';
-postPreviewDescription.textContent = 'Please, enter any description';
-inputDescription.addEventListener('input', changeDescription);
-
-const inputAuthor = document.getElementById('author-input')
-const postPreviewAuthor = document.getElementById('post-preview-author');
-
 function changeAuthor(event) {
     if (event.target.value !== '') {
         postPreviewAuthor.textContent = event.target.value;
@@ -67,37 +105,43 @@ function changeAuthor(event) {
     }
 }
 
-postPreviewAuthor.textContent = 'Enter author name';
-inputAuthor.addEventListener('input', changeAuthor);
-
-const inputDate = document.getElementById('date-input');
-const postPreviewDate = document.getElementById('post-preview-date');
-
 function changeDate(event) {
     if (event.target.value !== '') {
-        postPreviewDate.textContent = event.target.value;
+        const date = new Date(event.target.value);
+        postPreviewDate.textContent = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
     } else {
         postPreviewDate.textContent = '00/00/0000';
     }
 }
 
-postPreviewDate.textContent = '00/00/0000';
-inputDate.addEventListener('input', changeDate);
-
-
-
-authorImageInput = document.getElementById('author-image-input');
-authorImagePreview = document.getElementById('author-image')
-
-authorImageInput.addEventListener("change", updateImageDisplay);
-
-function updateImageDisplay(event) {
-    while (authorImagePreview.firstChild) {
-        authorImagePreview.removeChild(authorImagePreview.firstChild);
-    }
+function updateAuthorImageDisplay(event) {
     const curFiles = event.target.files;
-    const image = document.createElement("img");
-
-    image.src = window.URL.createObjectURL(curFiles[0]);
-    authorImagePreview.appendChild(image);
+    if (curFiles.length !== 0) {
+        authorImage.src = window.URL.createObjectURL(curFiles[0]);
+        uploadNewButton.hidden = false;
+        removeButton.hidden = false;
+        upload.hidden = true;
+        authorImagePreview.src = authorImage.src;
+    }
 }
+
+function removeAuthorImage(event) {
+    uploadNewButton.hidden = true;
+    removeButton.hidden = true;
+    upload.hidden = false;
+    authorImage.src = 'static/images/placeholder-image-round.svg';
+    authorImageInput.value = '';
+    authorImagePreview.src = 'static/images/author-image-preview.svg';
+}
+
+function uploadNewImage(event) {
+    authorImageInput.click();
+}
+
+function updateArticleImageDispaly(event) {
+
+}
+
+initListeners();
+
+
